82557d420e841050d51268e0aa3e7d7a9edb676b
adiharel <adi.harel10@gmail.com>
2024-03-22 12:37:44 +0200

    First commit

---
+++ src/test/java/org/instadi/instagrambe/InstagramBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.instadi.instagrambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
